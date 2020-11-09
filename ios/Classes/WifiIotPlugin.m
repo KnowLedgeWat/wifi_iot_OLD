2ce3b153420a5c90adb66438180f58a244e312d6
KnowLedgeWat <55449998+KnowLedgeWat@users.noreply.github.com>
2020-11-09 16:02:53 +0100

    Changed iot_wifi to iot_wifi_mySettings
    https://stackoverflow.com/questions/24102104/how-can-i-import-swift-code-to-objective-c

--- ios/Classes/WifiIotPlugin.m
+++ ios/Classes/WifiIotPlugin.m
@@ -1,5 +1,5 @@
 #import "WifiIotPlugin.h"
-#import <wifi_iot/wifi_iot-Swift.h>
+#import <iot_wifi_mySettings/iot_wifi_mySettings-Swift.h>
 
 @implementation WifiIotPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
